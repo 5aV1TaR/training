5dc95cb26f20987be88ec95625fbf25a50e7c35d
5aV1TaR <94852728+5aV1TaR@users.noreply.github.com>
2025-03-02 21:03:56 +0800

    Add files via upload

---
+++ SnakeGame/SnakeGame/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace SnakeGame
+{
+    partial class FrmSnakeGame
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TimerGameLoop = new System.Windows.Forms.Timer(this.components);
+            this.TimerFoodBlink = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // TimerGameLoop
+            // 
+            this.TimerGameLoop.Interval = 300;
+            this.TimerGameLoop.Tick += new System.EventHandler(this.TimerGameLoop_Tick);
+            // 
+            // TimerFoodBlink
+            // 
+            this.TimerFoodBlink.Tick += new System.EventHandler(this.TimerFoodBlink_Tick);
+            // 
+            // FrmSnakeGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 482);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmSnakeGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Змейка";
+            this.Load += new System.EventHandler(this.FrmSnakeGame_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FrmSnakeGame_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FrmSnakeGame_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer TimerGameLoop;
+        private System.Windows.Forms.Timer TimerFoodBlink;
+    }
+}
+
