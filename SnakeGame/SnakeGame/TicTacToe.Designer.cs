1fdf291558a98ccdfca3120cff4552a0f4bfcef2
x696X969x5aV1TaRx969X696x <x696X969x5aV1TaRx969X696x@yandex.ru>
2025-03-26 00:42:17 +0800

    тест 4

--- SnakeGame/SnakeGame/TicTacToe.Designer.cs
+++ SnakeGame/SnakeGame/TicTacToe.Designer.cs
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Indigo;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(99, 96);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Indigo;
+            this.panel2.Location = new System.Drawing.Point(117, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(99, 96);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Indigo;
+            this.panel3.Location = new System.Drawing.Point(222, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(99, 96);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Indigo;
+            this.panel4.Location = new System.Drawing.Point(222, 114);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(99, 96);
+            this.panel4.TabIndex = 4;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Indigo;
+            this.panel5.Location = new System.Drawing.Point(117, 114);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(99, 96);
+            this.panel5.TabIndex = 3;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Indigo;
+            this.panel6.Location = new System.Drawing.Point(12, 114);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(99, 96);
+            this.panel6.TabIndex = 2;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Indigo;
+            this.panel7.Location = new System.Drawing.Point(222, 216);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(99, 96);
+            this.panel7.TabIndex = 4;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.Indigo;
+            this.panel8.Location = new System.Drawing.Point(117, 216);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(99, 96);
+            this.panel8.TabIndex = 3;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.Indigo;
+            this.panel9.Location = new System.Drawing.Point(12, 216);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(99, 96);
+            this.panel9.TabIndex = 2;
             // 
             // TicTacToe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "TicTacToe";
             this.Text = "Крестики Нолики";
             this.ResumeLayout(false);
@@ -42,5 +132,15 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
     }
 }
